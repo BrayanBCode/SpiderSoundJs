4bf799760ef482f5e485a3e1717db7bb4a219fb9
Fausto <fdeleon@colgate.edu>
2024-01-03 00:51:33 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,9 @@
 import os, sys, discord
 from discord.ext import commands
-from discord import Embed
-
+import comandos.Musica 
 from dotenv import load_dotenv
+import pkgutil
+
 load_dotenv()
 intents = discord.Intents.default()
 intents.message_content = True
@@ -12,13 +13,13 @@ bot = commands.Bot(command_prefix = "=", intents=intents, help_command=None)
 
 @bot.event # Ejecutar la función cuando el bot se una a un canal de voz en un servidor
 async def on_voice_state_update(member, before, after):
-    if('Musica' in sys.modules):
-        await Musica.Event(member, before, after, bot)
+    if('comandos.Musica' in sys.modules):
+        await comandos.comandos.Musica.Event(member, before, after, bot)
 
 @bot.event
 async def on_ready():
-    if('Musica' in sys.modules):
-        await Musica.startup(bot)
+    if('comandos.Musica' in sys.modules):
+        await comandos.comandos.Musica.startup(bot)
     
 @bot.command() #Reinicia el bot con un comando
 async def restart(ctx):
@@ -29,10 +30,10 @@ async def restart(ctx):
 
 @bot.command()
 async def help(ctx):
-    embed = Embed(title="Guia de de comandos", description="En esta guia se nombraran los comandos implementados en el Bot.", color=0x7289DA)
-    if('Musica' in sys.modules):
+    embed = discord.Embed(title="Guia de de comandos", description="En esta guia se nombraran los comandos implementados en el Bot.", color=0x7289DA)
+    if('comandos.Musica' in sys.modules):
         embed.add_field(name=f"**{bot.command_prefix}play**", value=f"Para reproducir música, simplemente escribe **{bot.command_prefix}play** seguido del nombre de la canción, el artista o la URL de la canción que desees escuchar.", inline=False)
-        embed.add_field(name=f"**{bot.command_prefix}stop**", value=f"Para pausar la musica utilize **{bot.command_prefix}stop** una vez para reanudar la musica utilize **{bot.command_prefix}stop** nuevamente", inline=False)
+        embed.add_field(name=f"**{bot.command_prefix}stop**", value=f"Para pausar la comandos.Musica utilize **{bot.command_prefix}stop** una vez para reanudar la comandos.Musica utilize **{bot.command_prefix}stop** nuevamente", inline=False)
         embed.add_field(name=f"**{bot.command_prefix}skip**", value=f"Para saltear una cancion utilize **{bot.command_prefix}skip**, para saltear varias agrege un numero, ejemplo: **{bot.command_prefix}skip 3**", inline=False)
         embed.add_field(name=f"**{bot.command_prefix}queue**", value=f"Muestra la playlist y la cancion que se esta reproduciendo actualmente", inline=False)
         embed.add_field(name=f"**{bot.command_prefix}remove**", value=f"Quita de la playlist la cancion que el usuario desee ejemplo: **{bot.command_prefix}remove 5**", inline=False)
@@ -42,39 +43,39 @@ async def help(ctx):
     await ctx.send(embed=embed)
 
 
-#* Comandos Musica ---------------------------
-if('Musica' in sys.modules):
+#* Comandos comandos.Musica ---------------------------
+if('comandos.Musica' in sys.modules):
     @bot.command()
     async def play(ctx, *, command):
-        await Musica.AddSongs(ctx, command, bot)
+        await comandos.Musica.AddSongs(ctx, command, bot)
 
     @bot.command()
     async def p(ctx, *, command):
-        await Musica.AddSongs(ctx, command, bot)
+        await comandos.Musica.AddSongs(ctx, command, bot)
 
     @bot.command()
     async def stop(ctx):
-        await Musica.stop(ctx)
+        await comandos.Musica.stop(ctx)
 
     @bot.command()
     async def queue(ctx):
-        await Musica.queue(ctx, bot)
+        await comandos.Musica.queue(ctx, bot)
 
     @bot.command()
     async def skip(ctx, command: int = 1):
-        await Musica.skip(ctx, command)
+        await comandos.Musica.skip(ctx, command)
 
     @bot.command()
     async def clear(ctx):
-        await Musica.clear(ctx)
+        await comandos.Musica.clear(ctx)
 
     @bot.command()
     async def remove(ctx, command):
-        await Musica.remove(ctx, command)
+        await comandos.Musica.remove(ctx, command)
         
     @bot.command()
     async def loop(ctx):
-        await Musica.loop(ctx)
+        await comandos.Musica.loop(ctx)
 
 #* Comandos Gestion --------------------------
     
