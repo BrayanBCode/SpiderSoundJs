6777ba26b29fa59b0d67ee83e4c0ebc4c4d50fe6
BrayanBCode <134159765+BrayanBCode@users.noreply.github.com>
2024-01-24 17:34:14 -0300

    caca

--- Spiderbot/Main.py
+++ Spiderbot/Main.py
@@ -19,7 +19,7 @@ load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="-", intents=intents)
+bot = commands.Bot(command_prefix="=", intents=intents)
 
 bot.remove_command('help')
 
