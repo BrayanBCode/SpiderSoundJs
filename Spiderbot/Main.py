df930b68a1243da5295f6e67986f3191875a39ac
BrayanBCode <134159765+BrayanBCode@users.noreply.github.com>
2024-01-24 20:44:27 -0300

    Update Main.py

--- Spiderbot/Main.py
+++ Spiderbot/Main.py
@@ -27,7 +27,7 @@ bot.remove_command('help')
 
 @bot.event
 async def on_ready():
-    #HOlaaa esto es un test
+    #HOlaaa esto es un testa
     await Status()
     try:
         await bot.add_cog(Music_Ext(bot))
