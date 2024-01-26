96799403f80be85b56ded7d53089c40003c21353
BrayanBCode <134159765+BrayanBCode@users.noreply.github.com>
2024-01-25 21:25:14 -0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -21,7 +21,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="=", intents=intents)
 
-bot.remove_command('help')#
+bot.remove_command('help')
 
 #! eventos --------------------------------------------------------------------
 
