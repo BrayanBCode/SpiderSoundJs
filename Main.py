2ccfb1fd74026b50447eb57d14cfba11f63f0f46
Codú <134159765+BrayanBCode@users.noreply.github.com>
2024-02-17 20:10:02 -0300

    testing

--- Main.py
+++ Main.py
@@ -4,17 +4,18 @@ from dotenv import load_dotenv
 
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
+bot.load_extension('utils.cogs.musica')
+
 
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
-@bot.slash_command(name = "hello", description = "Say hello to the bot")
-async def hello(ctx):
-    await ctx.respond("Hey!")
+@bot.slash_command(name = "help", description = "Despliega el menu de ayuda")
+async def help(ctx):
+    await ctx.respond("Sin implementar")
 
-@bot.slash_command(name = "ping", description = "Muestra la latencia")
-async def ping(ctx): # a slash command will be created with the name "ping"
-    await ctx.respond(f"Pong! Latency is {bot.latency}")
+
+
 
 bot.run(os.getenv('TEST')) # run the bot with the token
---
+++ utils/cogs/musica.py
@@ -0,0 +1,55 @@
+import discord
+from discord.ext import commands
+from discord import option
+
+class musica(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    @discord.slash_command(name = "play", description = "Agrega y reproduce musica desde YT, puede utilizar una url, nombre de la cancion y url de Spotify")
+    async def play(self, ctx, url: str):
+        await ctx.respond("Sin implementar")
+
+    @discord.slash_command(name = "forceplay", description = "salta una o mas canciones")
+    async def forceplay(self, ctx, posición: int):
+        await ctx.respond("Sin implementar")
+
+    @discord.slash_command(name = "skip", description = "salta una o mas canciones")
+    @option('posición', int, description="Posición en la que se encuentra en la cola")
+    async def skip(self, ctx, posición: int = None):
+        await ctx.respond("Sin implementar")
+        
+    @discord.slash_command(name = "remove", description = "Quita una cancion de la cola a eleccion, vea la posicion de la cancion con /queue")
+    async def remove(self, ctx, posición: int):
+        await ctx.respond("Sin implementar")
+        
+    @discord.slash_command(name = "pause", description = "Pausa la reproduccion")
+    async def pause(self, ctx):
+        await ctx.respond("Sin implementar")
+        
+    @discord.slash_command(name = "resume", description = "reanuda la reproduccion")
+    async def resume(self, ctx):
+        await ctx.respond("Sin implementar")
+        
+    @discord.slash_command(name = "stop", description = "Detiene la reproduccion")
+    async def stop(self, ctx):
+        await ctx.respond("Sin implementar")     
+        
+    @discord.slash_command(name = "queue", description = "Muestra la cola de reproduccion")
+    async def queue(self, ctx):
+        await ctx.respond("Sin implementar")
+        
+    @discord.slash_command(name = "clear", description = "Limpia la cola")
+    async def clear(self, ctx):
+        await ctx.respond("Sin implementar")
+        
+    @discord.slash_command(name = "loop", description = "Activa o desactiva el loop de la cola")
+    async def loop(self, ctx):
+        await ctx.respond("Sin implementar")
+        
+    @discord.slash_command(name = "leave", description = "Desconecta el bot del canal de voz")
+    async def leave(self, ctx):
+        await ctx.respond("Sin implementar")
+        
+def setup(bot):
+    bot.add_cog(musica(bot))
