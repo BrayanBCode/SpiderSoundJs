105196b8f92aa141c224802b53181e062cb307f7
Fantasmartu <fantasmartu@gmail.com>
2024-03-15 03:32:25 -0300

    riot issue v2 la venganza
    se cambio un poco la logica y se agrego el hecho del icono

--- testosterona/test.py
+++ testosterona/test.py
@@ -1,11 +1,22 @@
 import requests
 
-
-def getsumm(summoner):
-    api_key = "RGAPI-f987fe75-ec7d-447e-a433-f73b4fb35b9d"
+class SummonerClass():
+    def __init__(self) -> None:
+        self.Summoner = None
     
-    api_url = f"https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner}"
-    api_url += '?api_key='+ api_key
-    resp = requests.get(api_url)
-    player_info = resp.json()
-    return(player_info["summonerLevel"])
+    def getsumm(self,summoner):
+        api_key = "RGAPI-f987fe75-ec7d-447e-a433-f73b4fb35b9d"
+        api_url = f"https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner}"
+        api_url += '?api_key='+ api_key
+        resp = requests.get(api_url)
+        player_info = resp.json()
+        self.Summoner = player_info
+
+
+    def setlvl(self):
+        lvl = self.Summoner["summonerLevel"]
+        return lvl
+
+    def seticon(self):
+        icon = self.Summoner["profileIconId"]
+        return f"https://ddragon.leagueoflegends.com/cdn/11.14.1/img/profileicon/{icon}.png"
--- utils/Commands/LolLvl.py
+++ utils/Commands/LolLvl.py
@@ -7,12 +7,16 @@ class GetLvl(commands.Cog):
     
     def __init__(self,bot):
         self.bot = bot
-
+    
     @discord.slash_command(name='lollvl', description='Te da tu nivel en lol')
     @option('invocador', str, description="Nombre de invocador")
     async def lollvl (self, ctx: ApplicationContext, summoner: str) -> None:
-        
-        await ctx.respond(embed=Embed(title="Sumoner Level", description=test.getsumm(summoner)))
+        test.SummonerClass.getsumm(summoner)
+        emb = Embed(title="Sumoner Level", description=test.SummonerClass.setlvl)
+        emb.set_image(test.SummonerClass.seticon(summoner))
+        await ctx.respond(embed=emb)
+
+    
 
 def setup(bot):
     bot.add_cog(GetLvl(bot))
