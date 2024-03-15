e3d8f34cba7904adf63855ba205ee94fb373151c
Codú <134159765+BrayanBCode@users.noreply.github.com>
2024-03-15 04:08:02 -0300

    Martu issue

--- testosterona/test.py
+++ testosterona/test.py
@@ -1,17 +1,13 @@
 import requests
 
 class SummonerClass():
-    def __init__(self) -> None:
-        self.Summoner = None
-    
-    def getsumm(self,summoner):
+    def __init__(self, summoner) -> None:
         api_key = "RGAPI-f987fe75-ec7d-447e-a433-f73b4fb35b9d"
         api_url = f"https://la2.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summoner}"
         api_url += '?api_key='+ api_key
         resp = requests.get(api_url)
         player_info = resp.json()
         self.Summoner = player_info
-
 
     def setlvl(self):
         lvl = self.Summoner["summonerLevel"]
--- utils/Commands/LolLvl.py
+++ utils/Commands/LolLvl.py
@@ -11,14 +11,11 @@ class GetLvl(commands.Cog):
     @discord.slash_command(name='lollvl', description='Te da tu nivel en lol')
     @option('invocador', str, description="Nombre de invocador")
     async def lollvl (self, ctx: ApplicationContext, invocador: str) -> None:
-        pepe = SummonerClass()
-        pepe.getsumm(invocador)
-        emb = Embed(title="Sumoner Sheet", description=pepe.setname())
-        emb.add_field(name=pepe.setlvl(),value="")
-        emb.set_image(pepe.seticon())
+        pepe = SummonerClass(invocador)
+        emb = Embed(title="Sumoner Sheet", description=pepe.setname(), color=0xce6300)
+        emb.add_field(name=pepe.setlvl(), value="")
+        emb.set_thumbnail(url=pepe.seticon())
         await ctx.respond(embed=emb)
-
-    
 
 def setup(bot):
     bot.add_cog(GetLvl(bot))
