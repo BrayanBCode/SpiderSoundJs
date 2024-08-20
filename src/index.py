fec9c63f0cd9bd57719a9063d84bbf6a261d423d
Codú <134159765+BrayanBCode@users.noreply.github.com>
2024-08-20 05:34:08 -0300

    feat: auto restart bot after manual shutdown

--- src/index.py
+++ src/index.py
@@ -1,10 +1,24 @@
 
 from base.classes.Bot import CustomBot
-import dotenv, os
+import dotenv, os, signal, sys
 
 dotenv.load_dotenv()
 
 # os.getenv("debug")
 
+def restart_bot():
+    """Reinicia el bot ejecutando el script de nuevo."""
+    python = sys.executable
+    os.execl(python, python, *sys.argv)
+
+def signal_handler(sig, frame):
+    """Maneja la señal SIGINT (Ctrl + C) y reinicia el bot."""
+    print("Reiniciando el bot...")
+    restart_bot()
+
 if __name__ == "__main__":
+    # Registra el manejador de señales para SIGINT
+    signal.signal(signal.SIGINT, signal_handler)
+
+    # Inicializa y ejecuta el bot
     bot = CustomBot(command_prefix="=", debug=False)
