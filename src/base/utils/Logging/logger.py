cd0e622668b68485306bee818da6540560a7fb7f
Codú <134159765+BrayanBCode@users.noreply.github.com>
2024-08-23 22:42:21 -0300

    fix(logger): send() def are print() now

--- src/base/utils/Logging/logger.py
+++ src/base/utils/Logging/logger.py
@@ -1,17 +1,12 @@
-from pathlib import Path
 import traceback
+from pathlib import Path
 import discord
 
 class logger:
     """
     Clase base para los mensajes de log.
     """
-    def __init__(
-            self, title: str, 
-            message: str, 
-            ForeColor: str, 
-            color: discord.Color
-            ):
+    def __init__(self, title: str, message: str, ForeColor: str, color: discord.Color):
         self.title = title
         self.message = message
         self.color = color
@@ -19,24 +14,25 @@ class logger:
 
     async def send(self, interaction: discord.Interaction):
         try:
-            await interaction.channel.send(embed=discord.Embed(
+            await interaction.followup.send(embed=discord.Embed(
                 title=self.title,
                 description=self.message,
                 color=self.color
             ))
         except Exception as e:
-            self.log(e, traceback)
+            self.log(e)
 
-    def send(self):
+    def print(self):
         try:
-            print(f"{self.ForeColor}[{self.__class__.__name__[3:]}] {self.title} - {self.message}")
+            print(f"{self.ForeColor}[{self.__class__.__name__[3:]}] {self.title} \n {self.message}")
         except Exception as e:
-            self.log(e, traceback)
+            self.log(e)
 
-    def log(self, err: Exception, tb: traceback):
+    def log(self, err: Exception):
+        print(f"[{self.__class__.__name__[3:]}] {err}: \n{traceback.format_exc()}\n ------------------ \n")
         self.create_directory_if_not_exists("logs")
         with open("logs/log.txt", "a") as log_file:
-            log_file.write(f"[{self.__class__.__name__[3:]}] {err}: \n{tb.format_exc()}\n")
+            log_file.write(f"[{self.__class__.__name__[3:]}] {err}: \n{traceback.format_exc()}\n ------------------ \n")
 
     @staticmethod
     def create_directory_if_not_exists(directory):
