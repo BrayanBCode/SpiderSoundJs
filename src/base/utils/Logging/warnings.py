5f9d07299e268455deeb9073451423164f558f0f
Codú <134159765+BrayanBCode@users.noreply.github.com>
2024-08-23 22:41:16 -0300

    feat: Debug Logger warning

--- src/base/utils/Logging/warnings.py
+++ src/base/utils/Logging/warnings.py
@@ -3,8 +3,9 @@ import discord
 
 
 class warnings(Enum):
-    Error = discord.Color.default()
-    Aviso = discord.Color.default()
-    Exitoso = discord.Color.default()
-    Info = discord.Color.default()
+    Error = discord.Color.red()
+    Aviso = discord.Color.yellow()
+    Exitoso = discord.Color.green()
+    Info = discord.Color.blue()
+    Debug = discord.Color.blurple()
 
