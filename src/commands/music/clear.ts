9e1951b7a7c13ebfc02df7e7bb9c18fdde6cf437
Codú <134159765+BrayanBCode@users.noreply.github.com>
2025-02-06 00:26:11 -0300

    feat: Se implementaron nuevos comandos - Musica
    description: Se implementaron los comandos /clean y /volume
    warn: falta revision y testeo

---
+++ src/commands/music/clear.ts
@@ -0,0 +1,39 @@
+import { SlashCommandBuilder } from "discord.js";
+import { Command } from "../../class/Commands";
+
+export default new Command({
+    data: {
+        command: new SlashCommandBuilder()
+            .setName("clear")
+            .setDescription("Limpia la lista de reprodución"),
+
+        category: "Music"
+    },
+    execute: async (client, interaction) => {
+        if (!interaction.guildId) return
+
+        await interaction.deferReply()
+
+        const player = client.Tools.getPlayer(interaction.guildId)
+
+        const ErrMessage = client.Tools.createEmbedTemplate()
+            .setDescription("La lista ya esta vacia. Utiliza /play para escuchar nuevamente")
+
+        if (!player) return await interaction.followUp({
+            embeds: [
+                ErrMessage
+            ]
+        })
+
+        player.queue.utils.destroy().then(async () => {
+            await interaction.followUp({
+                embeds: [
+                    client.Tools.createEmbedTemplate()
+                        .setDescription("🧹💨 Se borro la lista de reprodución")
+                ]
+            })
+        })
+
+    }
+
+})
