28424f8848b6b828a0290084c0433b7326cab3c8
Codú <134159765+BrayanBCode@users.noreply.github.com>
2025-05-15 22:15:40 -0300

    update: sistema de desconexión por inactividad

--- src/Events/discord/voiceStateUpdate.ts
+++ src/Events/discord/voiceStateUpdate.ts
@@ -1,131 +1,88 @@
-import { Guild, GuildMember, TextChannel, VoiceState } from "discord.js";
+import { VoiceState } from "discord.js";
 import { BotClient } from "../../class/BotClient.js";
 import { BaseDiscordEvent } from "../../class/events/BaseDiscordEvent.js";
-import { setTimeout } from "node:timers";
 import logger from "../../class/logger.js";
-import { createEmptyEmbed } from "../../utils/tools.js";
+
 
 export default class VoiceStateUpdate extends BaseDiscordEvent<"voiceStateUpdate"> {
-    private inactivityTimeouts: Map<string, NodeJS.Timeout> = new Map();
-
     name: "voiceStateUpdate" = "voiceStateUpdate";
+    InactivityManager = new Map<string, NodeJS.Timeout>()
+
 
     execute(client: BotClient, oldState: VoiceState, newState: VoiceState): void | Promise<void> {
-        this.disconnectByInactivity(client, oldState, newState);
+        if (this.didBotChannelUpdate(client, oldState, newState) && this.isBotAlone(client, newState)) {
+            this.disconectByInactivity(client, oldState, newState)
+        }
+
+
     }
 
-    async disconnectByInactivity(client: BotClient, oldState: VoiceState, newState: VoiceState) {
-        const guild = oldState.guild || newState.guild;
+    private isClient(client: BotClient, oldState: VoiceState, newState: VoiceState) {
+        return newState.member?.user.id === client.user!.id
+    }
 
-        // Verificar si el bot fue movido a un nuevo canal
-        const botWasMoved = oldState.channelId !== newState.channelId && newState.channel?.members.has(client.user!.id);
+    /**
+     * Verifica si el bot está solo en su canal de voz.
+     * 
+     * se recomienda llamar {@link didBotChannelUpdate} antes
+     * 
+     * @example
+     * if (this.didBotChannelUpdate(client, oldState, newState)) {
+     *      this.isBotAlone(client, newState)
+     * }
+     * 
+     * @param client Cliente del bot.
+     * @param newState Estado de voz actualizado.
+     * @returns `true` si el bot está solo, `false` si hay más miembros o no se pudo determinar.
+     */
+    private isBotAlone(client: BotClient, newState: VoiceState) {
 
-        if (botWasMoved) {
-            logger.info(`El bot fue movido al canal ${newState.channel?.id} en el servidor: ${guild.name}`);
+        const player = client.getPlayer(newState.guild.id)
 
-            // Verificar si el canal está vacío al momento del movimiento
-            const isNewChannelEmpty = newState.channel!.members.filter((member: GuildMember) => !member.user.bot).size === 0;
+        if (!player?.voiceChannelId) {
+            logger.error(`[isBotAlone] No se pudo obtener "player" de ${newState.guild.name}`)
+            return false;
+        }
 
-            if (isNewChannelEmpty) {
-                logger.info(`El bot está solo en el nuevo canal. Configurando temporizador de desconexión.`);
-                this.startInactivityTimer(client, guild, newState.channel!);
+        const channel = client.getVoiceChannel(player.voiceChannelId)
 
-            } else {
-                logger.info(`Hay usuarios en el nuevo canal, no se configurará el temporizador.`);
+        if (!channel) {
+            logger.error(`[isBotAlone] No se pudo obtener "Voice Channel" de ${newState.guild.name}`)
+            return false;
+        }
 
+        return channel.members.size <= 1;
+    }
+
+    private didBotChannelUpdate(client: BotClient, oldState: VoiceState, newState: VoiceState) {
+        const player = client.getPlayer(oldState.guild.id ?? newState.guild.id)
+
+        return oldState.channelId === player?.voiceChannelId || newState.channelId === player?.voiceChannelId
+    }
+
+    private disconectByInactivity(client: BotClient, oldState: VoiceState, newState: VoiceState) {
+        const GuildID = oldState.guild.id ?? newState.guild.id
+        const timeout = this.InactivityManager.get(GuildID)
+
+
+        if (timeout) timeout.close()
+        this.InactivityManager.set(GuildID, setTimeout(() => {
+            if (this.didBotChannelUpdate(client, oldState, newState) && this.isBotAlone(client, newState)) {
+                const player = client.getPlayer(GuildID)
+
+                if (!player) return
+
+                player.destroy("[VoiceStateUpdate] Disconnected by Inactivity", true)
+                client.playerMessage.delete(GuildID).catch(() => {
+                    logger.warn("[VoiceStateUpdate] Error al eliminar el mensaje")
+
+                })
+                client.playerMessage.MessageContainer.delete(GuildID)
+
+                logger.info(`[VoiceStateUpdate] Desconexion por inactividad en "${newState.guild.name}"`)
             }
+        }, 15000))
 
-            return;
-        }
-
-        // Validar que el bot esté en un canal de voz
-        const botChannel = oldState.channel || newState.channel;
-        if (!botChannel || !botChannel.members.has(client.user!.id)) {
-            // logger.debug("[disconnectByInactivity] El bot no está en un canal de voz, Return.");
-            return;
-        }
-
-        // Verificar si el canal quedó vacío después de que un usuario se movió o desconectó
-        const isChannelEmpty =
-            botChannel.members.filter((member: GuildMember) => !member.user.bot).size === 0;
-
-        if (isChannelEmpty) {
-            logger.info(`[disconnectByInactivity] El canal de voz está vacío en el servidor: ${guild.name}. Configurando temporizador de desconexión.`);
-            this.startInactivityTimer(client, guild, botChannel);
-        } else {
-            // Si el canal no está vacío, eliminar el temporizador existente
-            if (this.inactivityTimeouts.has(guild.id)) {
-                clearTimeout(this.inactivityTimeouts.get(guild.id)!);
-                this.inactivityTimeouts.delete(guild.id);
-                logger.info(`[disconnectByInactivity] Detectada actividad en el canal. Temporizador eliminado para el servidor: ${guild.name}`);
-            }
-        }
     }
-
-    // Método auxiliar para iniciar el temporizador de desconexión
-    private startInactivityTimer(client: BotClient, guild: any, channel: any) {
-        // Eliminar temporizador previo, si existe
-        if (this.inactivityTimeouts.has(guild.id)) {
-            clearTimeout(this.inactivityTimeouts.get(guild.id)!);
-            this.inactivityTimeouts.delete(guild.id);
-            logger.info(`[startInactivityTimer] Temporizador previo eliminado para el servidor: ${guild.name}`);
-        }
-
-        // Configurar nuevo temporizador de desconexión
-        this.inactivityTimeouts.set(
-            guild.id,
-            setTimeout(async () => {
-                logger.info(`[startInactivityTimer] Ejecutando desconexión por inactividad para el servidor: ${guild.name}`);
-
-                try {
-                    const player = client.lavaManager.getPlayer(guild.id);
-                    if (!player || !player.playing) {
-                        logger.warn(`[startInactivityTimer] No se encontró un reproductor activo para el servidor: ${guild.name}`);
-                        return;
-                    }
-
-                    let msg = client.lavaManager.playingMessageController.getMessage(guild.id);
-                    const textChannel = msg
-                        ? (msg.channel as TextChannel)
-                        : (client.channels.cache.get(player.textChannelId!) as TextChannel | undefined);
-
-                    // Desconectar el reproductor
-                    await player.disconnect();
-                    await player.destroy("Destoyed by Inactivity", true)
-                    // client.playerMessage.delete(player.guildId)
-
-                    logger.info(`[startInactivityTimer] Reproductor desconectado para el servidor: ${guild.name}`);
-
-                    // Eliminar el mensaje de control si existe
-                    if (msg) {
-                        try {
-                            await msg.delete();
-                            // client.lavaManager.destroyGuildMessage(player.guildId);
-                            logger.info(`[startInactivityTimer] Mensaje de control eliminado para el servidor: ${guild.name}`);
-                        } catch (err) {
-                            logger.error(`[startInactivityTimer] Error al eliminar el mensaje en el servidor: ${guild.name}:`, err);
-                        }
-                    }
-
-                    // Notificar al canal de texto
-                    if (textChannel) {
-                        const embed = createEmptyEmbed()
-                            .setAuthor({ name: "Desconexión por inactividad" });
-                        await textChannel.send({ embeds: [embed] });
-
-                        logger.info(`[startInactivityTimer] Notificación enviada al canal de texto en el servidor: ${guild.id}`);
-                    }
-                } catch (error) {
-                    logger.error(`[startInactivityTimer] Error durante la desconexión por inactividad en el servidor: ${guild.name}:`, error);
-                } finally {
-                    // Eliminar el temporizador una vez completado
-                    this.inactivityTimeouts.delete(guild.id);
-                    logger.info(`[startInactivityTimer] Temporizador limpiado para el servidor: ${guild.name}`);
-                }
-            }, 15000)
-        );
-    }
-
-
 
 }
