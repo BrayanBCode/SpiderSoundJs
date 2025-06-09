ee110bc93d085ba208796cced2f792dd066720fa
Codú <134159765+BrayanBCode@users.noreply.github.com>
2025-06-09 03:07:13 -0300

    refact: Remoción de log

--- src/core/events/lavalinkManager/playerSocketClose.ts
+++ src/core/events/lavalinkManager/playerSocketClose.ts
@@ -8,7 +8,7 @@ export default class trackError extends BaseLavalinkManagerEvents<"playerSocketC
     name: "playerSocketClosed" = "playerSocketClosed";
     execute(client: BotClient, player: Player, payload: WebSocketClosedEvent): void {
         logger.warn("player Socket Closed", client.getGuild(player.guildId)?.name)
-        logger.warn(payload)
+        // logger.warn(payload)
     }
 
 
